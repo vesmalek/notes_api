515339e379b30b22c68e6413aaee0d942a53de7d
Ismail Malik <vesmalick@gmail.com>
2026-05-27 23:15:16 +0300

    created The Pydantic models (create and update)

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Hello!")
+from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+# DONE: The Pydantic models (create and update)
+
+class NoteCreate(BaseModel):
+    title: str
+    content: str
+    tag: str = "general"
+    pinned: bool = False
+    archived: bool = False
+
+class NoteUpdate(BaseModel):
+    title: str
+    content: str
+    tag: str = "general"
+    pinned: bool = False
+    archived: bool = False
+
+# TODO: In-memory storage setup with auto-incrementing ID
+
+# TODO: Helper function for finding a note by ID
+
